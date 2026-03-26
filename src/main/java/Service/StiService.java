b8418df8fbec7e4c14cd93bd996d83de09fc4f1b
OswaldLim <limchuanhe@gmail.com>
2026-03-26 16:55:46 +0800

    Added loading animation and background threading for accessing database for smoother UI transitions

--- src/main/java/Service/StiService.java
+++ src/main/java/Service/StiService.java
@@ -4,38 +4,43 @@ import DBHandling.StiDatabase;
 import Models.StiEntry;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StiService {
-    public List<StiEntry> searchByName(List<StiEntry> data, String keyword) {
-        if (keyword == null || keyword.isBlank()) return data;
+    public ArrayList<StiEntry> searchByName(List<StiEntry> data, String keyword) {
+        if (keyword == null || keyword.isBlank()) return new ArrayList<>(data);
 
         return data.stream()
-                .filter(sti -> sti.getName().toLowerCase()
-                        .contains(keyword.toLowerCase()))
-                .toList();
+                .filter(sti -> sti.getName() != null &&
+                        sti.getName().toLowerCase().contains(keyword.toLowerCase()))
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public List<StiEntry> searchBySymptoms(List<StiEntry> data, String keyword) {
-        if (keyword == null || keyword.isBlank()) return data;
+    public ArrayList<StiEntry> searchBySymptoms(List<StiEntry> data, String keyword) {
+        if (keyword == null || keyword.isBlank()) return new ArrayList<>(data);
 
         return data.stream()
-                .filter(sti -> sti.getSymptoms().toLowerCase()
-                        .contains(keyword.toLowerCase()))
-                .toList();
+                .filter(sti -> sti.getSymptoms() != null &&
+                        sti.getSymptoms().toLowerCase().contains(keyword.toLowerCase()))
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public List<StiEntry> searchByRiskLevel(List<StiEntry> data, String keyword) {
-        if (keyword == null || keyword.isBlank()) return data;
+    public ArrayList<StiEntry> searchByRiskLevel(List<StiEntry> data, String keyword) {
+        if (keyword == null || keyword.isBlank()) return new ArrayList<>(data);
 
         try {
             int risk = Integer.parseInt(keyword);
 
             return data.stream()
                     .filter(sti -> sti.getRiskLevel() == risk)
-                    .toList();
+                    .collect(Collectors.toCollection(ArrayList::new));
 
         } catch (NumberFormatException e) {
-            return List.of();
+            return new ArrayList<>(); // return empty ArrayList
         }
+    }
+
+    public static ArrayList<StiEntry> getAll(){
+        return StiDatabase.getAll();
     }
 }
