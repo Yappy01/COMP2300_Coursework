f7124d8101424d0d95306893cc4688e302659d1d
OswaldLim <limchuanhe@gmail.com>
2026-04-13 16:33:21 +0800

    Added a connection pool

--- src/main/java/utils/DBConnection.java
+++ src/main/java/utils/DBConnection.java
@@ -1,29 +1,41 @@
 package utils;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.github.cdimascio.dotenv.Dotenv;
 import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
 
 public class DBConnection {
     private static final Dotenv dotenv = Dotenv.load();
+    private static final HikariDataSource ds;
+
+    static {
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl(dotenv.get("DB_URL"));
+        config.setUsername(dotenv.get("DB_USER"));
+        config.setPassword(dotenv.get("DB_PASSWORD"));
+
+        config.setMaximumPoolSize(10); // 🔥 important
+        config.setMinimumIdle(2);
+
+        ds = new HikariDataSource(config);
+    }
+
 
     public static Connection getConnection() throws SQLException {
         String url = dotenv.get("DB_URL");
-        String user = dotenv.get("DB_USER");
-        String password = dotenv.get("DB_PASSWORD");
-
         if (url == null){
             throw new RuntimeException("DB URL not set");
         }
 
-        return DriverManager.getConnection(url, user, password);
+        return ds.getConnection();
     }
 
     public static Cloudinary cloudinary() {
-//        System.out.println(dotenv.get("CLOUD_URL"));
         Cloudinary cloudinary = new Cloudinary(dotenv.get("CLOUD_URL"));
         return cloudinary;
     }
